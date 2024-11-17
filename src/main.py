d23852643e6d05406b454c7fed56c550f74dc0b5
Igor Jakus <igorjakus@protonmail.com>
2024-11-17 03:06:59 +0100

    feat: Proposition of main loop

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import time
+from model import Model
+from connect import Connection
+from sms import send_email_risk, send_email_seizure
+
+model = Model()
+connection = Connection()
+
+while True:
+    data = connection.get_data()
+    prediction = model.predict(data)
+
+    if prediction == 1:
+        send_email_risk()
+    elif prediction == 2:
+        send_email_seizure()
+    
+    time.sleep(5)
