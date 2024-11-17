ca0b9d2e1be95a5623ef797f99889a092d28d121
Igor Jakus <igorjakus@protonmail.com>
2024-11-17 02:28:44 +0100

    feat: Add svm2

--- src/connect.py
+++ src/connect.py
@@ -9,7 +9,7 @@ def plot_1s(data_1s, timestamps):
     timestamps = np.arange(timestamps)
 
     # Create a figure with 8 subplots
-    fig, axes = plt.subplots(8, 1, figsize=(10, 15))
+    _, axes = plt.subplots(8, 1, figsize=(10, 15))
 
     # Plot each row of the array on a separate subplot
     for i in range(8):
@@ -23,17 +23,18 @@ def plot_1s(data_1s, timestamps):
     plt.show()
 
 
-while(1):
+while True:
     db, status = bb.db_connect()
     if status:
         data = db.get_mne()
         mne_data = data[next(iter(data))] 
 
 
-
     channels_of_electrodes = ['F3', 'F4', 'C3', 'C4', 'P3', 'P4', 'O1', 'O2']
     mne_data.pick(channels_of_electrodes).filter(1, 40)
-    #mne_data.pick(channels_of_electrodes)
+    
+    # mne_data.pick(channels_of_electrodes)
+    
     data = mne_data.get_data()
     np_data = np.array(data)
     data_last_1s = np_data[:, -1000:] # 250 is a sfreq
@@ -44,5 +45,5 @@ while(1):
     plot_1s(data_last_1s, 1000)
     print()
 
-    sleep(10)
+    sleep(5)
     
