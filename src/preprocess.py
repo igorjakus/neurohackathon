626a0988f87b53a807e422d02f6f7d73fb04ad64
Igor Jakus <igorjakus@protonmail.com>
2025-01-15 11:48:36 +0100

    feat: Shuffle data in KMeans, add new options in preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -46,14 +46,19 @@ def rename_labels(labels: np.array) -> np.array:
 
     # y_binary = y.apply(lambda label: 1 if label in [1, 2, 3] else 0)
 
-def get_prepared_data(oversample = False) -> Tuple[np.array, np.array]:
-    """ Labels before renaming:
+def get_prepared_data(oversample = False, standarize=True, shuffle=False) -> Tuple[np.array, np.array]:
+    """ Labels before relabeling:
     5 - eyes open, means when they were recording the EEG signal of the brain the patient had their eyes open
     4 - eyes closed, means when they were recording the EEG signal the patient had their eyes closed
     3 - Yes they identify where the region of the tumor was in the brain and recording the EEG activity from the healthy brain area
     2 - They recorded the EEG from the area where the tumor was located
     1 - Recording of seizure activity
+
+    Labels after relabeling:
+    0 - No seizure
+    1 - Seizure
     """
+
     # Get preprocessed data
     data = preprocess_data(pd.read_csv("../data/Epileptic_Seizure_Recognition.csv"))
     X = data["values"]
@@ -67,10 +72,14 @@ def get_prepared_data(oversample = False) -> Tuple[np.array, np.array]:
         sm = SMOTE(random_state=42)
         X,y = sm.fit_resample(X,y)
 
-    # Normalize the data
-    scaler = StandardScaler()
+    # Standarize the data
+    if standarize:
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
 
-    # Fit the scaler on the data
-    X = scaler.fit_transform(X)
+    # Shuffle the data
+    if shuffle:
+        indices = np.random.permutation(len(X))
+        X, y = X[indices], y[indices]
 
     return X, y
