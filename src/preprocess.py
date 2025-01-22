1b6203f2c3f45daae3fcbc1b84cf86780f73567a
Igor Jakus <igorjakus@protonmail.com>
2025-01-22 12:03:14 +0100

    feat: Introduct time series preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
+from tslearn.preprocessing import TimeSeriesScalerMinMax
 
 
 def preprocess_data(dataset: pd.DataFrame) -> np.array:
@@ -44,9 +45,8 @@ def rename_labels(labels: np.array) -> np.array:
     transformed_labels[(labels == 1)] = 1  
     return transformed_labels # all labels except 1 are marked as not having a seizure
 
-    # y_binary = y.apply(lambda label: 1 if label in [1, 2, 3] else 0)
 
-def get_prepared_data(oversample = False, standarize=True, shuffle=False) -> Tuple[np.array, np.array]:
+def get_prepared_data(oversample = False, shuffle=False, relabel=True, standard_scaler=True, time_series_scaler=False, time_series_reshape=False) -> Tuple[np.array, np.array]:
     """ Labels before relabeling:
     5 - eyes open, means when they were recording the EEG signal of the brain the patient had their eyes open
     4 - eyes closed, means when they were recording the EEG signal the patient had their eyes closed
@@ -55,25 +55,40 @@ def get_prepared_data(oversample = False, standarize=True, shuffle=False) -> Tup
     1 - Recording of seizure activity
 
     Labels after relabeling:
-    0 - No seizure
-    1 - Seizure
+    0 - No seizure (labels 2, 3, 4, 5 are considered as no seizure)
+    1 - Seizure    (label 1)
     """
-
+    if standard_scaler and time_series_scaler:
+        raise ValueError("Both standard_scaler and time_series_scaler cannot be True at the same time")
+    
+    if time_series_reshape ^ time_series_scaler:
+        raise ValueError("Both time_series_reshape and time_series_scaler should be True or False at the same time")
+ 
     # Get preprocessed data
     data = preprocess_data(pd.read_csv("../data/Epileptic_Seizure_Recognition.csv"))
     X = data["values"]
     y = data["labels"]
 
+    """ Time series methods (5 labels categorisation) """
+    if time_series_reshape:
+        X = X.reshape(X.shape[0], X.shape[1], 1)
+
+    if time_series_scaler:
+        scaler = TimeSeriesScalerMinMax()
+        X = scaler.fit_transform(X)
+
+    """ Conventional methods (2 labels categorisation) """
     # Rename labels as described in rename_labels function
-    y = rename_labels(data["labels"])
+    if relabel:
+        y = rename_labels(y)
 
     # Oversample the data
     if oversample:
         sm = SMOTE(random_state=42)
         X,y = sm.fit_resample(X,y)
 
-    # Standarize the data
-    if standarize:
+    # Standardize the data
+    if standard_scaler:
         scaler = StandardScaler()
         X = scaler.fit_transform(X)
 
